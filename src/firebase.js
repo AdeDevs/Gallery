f7e1c7d3bf4855941997ecbe05322e3fe4ab8644
AdeDevs <adeyemiakinyemi01@gmail.com>
2023-02-05 00:21:38 +0100

    Updated

---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAtQbqkE3OERWAjfEoswEuEIO5lmPTQbUs",
+  authDomain: "gimmikgallery.firebaseapp.com",
+  projectId: "gimmikgallery",
+  storageBucket: "gimmikgallery.appspot.com",
+  messagingSenderId: "863625581798",
+  appId: "1:863625581798:web:91c719fc7d88d9ba051727"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
