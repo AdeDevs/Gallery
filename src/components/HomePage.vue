9c95e42139fa038f6922a4a54245be8d76d56d39
AdeDevs <adeyemiakinyemi01@gmail.com>
2023-02-04 23:59:49 +0100

    Updated

--- src/components/HomePage.vue
+++ src/components/HomePage.vue
@@ -1,12 +1,15 @@
 <template>
     <div class="container">
+        <header>
+            <h1>This Gallery Was Built By <a href="https://linktr.ee/adedevs">{{ developer }}</a></h1>
+        </header>
         <img :src="image" alt="image" />
         <p>{{ museFirst }} {{ museLast }}</p>
         <br>
         <button @click="shuffleNextImage()">Prev</button>
         <button @click="shuffleNextImage()">Next</button>
         <footer>
-            <h1>This Gallery Was Built By <a href="#">{{ developer }}</a></h1>
+            <h1>This Gallery Was Built By <a href="https://linktr.ee/adedevs">{{ developer }}</a></h1>
         </footer>
     </div>
 </template>
