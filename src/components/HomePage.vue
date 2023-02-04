24c2bb74c8d4e8ef59d9c08ad13e75a219fd09aa
AdeDevs <adeyemiakinyemi01@gmail.com>
2023-02-04 23:27:07 +0100

    Updated

--- src/components/HomePage.vue
+++ src/components/HomePage.vue
@@ -1,15 +1,28 @@
 <template>
     <div>
         <h1>My Gallery HomePage Was Built By {{ developer }}</h1>
+        <img :src="image" alt="image" />
+        <br>
+        <button @click="shuffleNextImage()">Next</button>
     </div>
 </template>
 
 <script>
 export default {
-    name: "Homepage",
+    name: "HomePage",
         data() {
             return {
-                developer: "Adeyemi"
+                developer: "Adeyemi",
+                image: "https://randomuser.me/api/portraits/men/19.jpg",
+            }
+        },
+        methods: {
+            async shuffleNextImage() {
+                const res = await fetch("https://randomuser.me/api/");
+                const { results } = await res.json();
+                
+                this.developer = 'Reece James';
+                this.image = results[0].picture.large
             }
         }
     }
