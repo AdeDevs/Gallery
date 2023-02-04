e99f5851728bc343f28c43026c04be625b683b0c
AdeDevs <adeyemiakinyemi01@gmail.com>
2023-02-04 23:07:44 +0100

    first commit

---
+++ src/components/HomePage.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        <h1>My Gallery HomePage Was Built By {{ developer }}</h1>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "Homepage",
+        data() {
+            return {
+                developer: "Adeyemi"
+            }
+        }
+    }
+</script>
