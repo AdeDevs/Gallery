720ba2b581f083b5fd2777da34589b248ebfa83b
AdeDevs <adeyemiakinyemi01@gmail.com>
2023-11-02 21:50:54 +0100

    ok

--- src/components/HomePage.vue
+++ src/components/HomePage.vue
@@ -19,7 +19,7 @@ export default {
     name: "HomePage",
         data() {
             return {
-                developer: "AdeDevs",
+                developer: "AdeDevs" ,
                 image: "https://randomuser.me/api/portraits/men/19.jpg",
                 museFirst: "Jon",
                 museLast: "Doe"
